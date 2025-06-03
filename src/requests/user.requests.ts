f532d36d6bac9422f095b2d1299082c285d14896
abhaseeb-neslit <abdulhaseeb@nesl-it.com>
2025-06-04 00:10:34 +0500

    fix/fixed urls

--- src/requests/user.requests.ts
+++ src/requests/user.requests.ts
@@ -3,7 +3,7 @@ import { IUser } from "@/types";
 import { AxiosPromise } from "axios";
 
 // ENDPOINT URLS
-const meUrl = "/auth/web/me";
+const meUrl = "/user/me";
 
 // REQUESTS
 export const loadUser = (): AxiosPromise<{ user: IUser }> => api.get(meUrl);
